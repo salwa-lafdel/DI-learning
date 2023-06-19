9740160690952a66f1629b78ef5ce4d17ac7b789
Salwa <salwalfdal32@gmail.com>
2023-06-19 19:16:39 +0100

    first commit

--- week11/day4/dayli_chal/index.sql
+++ week11/day4/dayli_chal/index.sql
@@ -1,21 +1,29 @@
-CREATE FUNCTION calculate_order_total(order_id INT) RETURNS DECIMAL(10,2)
+-- Create product_orders table
+CREATE TABLE product_orders (
+  order_id INT PRIMARY KEY,
+  order_date DATE,
+  customer_id INT 
+
+);
+
+-- Create items table
+CREATE TABLE items (
+  item_id INT PRIMARY KEY,
+  order_id INT,
+  item_name VARCHAR(255),
+  price DECIMAL(10, 2) 
+  FOREIGN KEY (order_id) REFERENCES product_orders (order_id)
+);
+CREATE FUNCTION calculate_order_total(order_id INT) RETURNS DECIMAL(10, 2)
 BEGIN
-    DECLARE total DECIMAL(10,2);
+    DECLARE total DECIMAL(10, 2);
     SELECT SUM(price) INTO total
     FROM items
     WHERE order_id = order_id;
     RETURN total;
 END;
 
-CREATE FUNCTION calculate_user_order_total(user_id INT, order_id INT) RETURNS DECIMAL(10,2)
-BEGIN
-    DECLARE total DECIMAL(10,2);
-    SELECT SUM(i.price) INTO total
-    FROM items AS i
-    JOIN product_orders AS po ON i.order_id = po.order_id
-    WHERE po.user_id = user_id AND po.order_id = order_id;
-    RETURN total;
-END;
+
 
 
 
